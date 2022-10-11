7834f8951681f058161eb98b40c14fa4628e79bf
CAS <78571139+CAS-14@users.noreply.github.com>
2022-10-11 13:02:56 -0400

    add things

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template
+import os
+import json
+
+if "instance" not in os.listdir():
+    os.mkdir("instance")
+
+def get_data():
+    with open("instance/data.json", "r") as f:
+        data = json.load(f)
+
+    return data
+
+def set_data(data):
+    with open("instance/data.json", "w") as f:
+        json.dump(data, f, indent=4)
+
+def create_post(title: str, author: str, content: str):
+    data = get_data()
+    post_id = max(data["posts"].keys()) + 1
+
+    data["posts"][post_id] = {
+        "title": title,
+        "author": author,
+        "content": content
+    }
+
+    set_data(data)
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/create")
+def create():
+    return render_template("create.html")
